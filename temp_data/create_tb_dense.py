027a3fb9c41d26e9fafd633d275f79c3c2f0797a
Max Moser <maxmoser.15@gmail.com>
2025-09-26 16:00:14 +0200

    add script for creating train, dev, test split for tb-dense

--- temp_data/create_tb_dense.py
+++ temp_data/create_tb_dense.py
@@ -75,8 +75,34 @@ trans_rules = {  # rule 1
     # ("simultaneous", "vague"): ["vague"],
 }
 
+devDocs = [
+    "APW19980227.0487.tml",
+    "CNN19980223.1130.0960.tml",
+    "NYT19980212.0019.tml",
+    "PRI19980216.2000.0170.tml",
+    "ed980111.1130.0089.tml",
+]
 
-def process_tb_dense(trans_rules: dict[tuple[str, str], list[str]] = trans_rules) -> list:
+testDocs = [
+    "APW19980227.0489.tml",
+    "APW19980227.0494.tml",
+    "APW19980308.0201.tml",
+    "APW19980418.0210.tml",
+    "CNN19980126.1600.1104.tml",
+    "CNN19980213.2130.0155.tml",
+    "NYT19980402.0453.tml",
+    "PRI19980115.2000.0186.tml",
+    "PRI19980306.2000.1675.tml",
+]
+
+
+def process_tb_dense(
+    trans_rules: dict[tuple[str, str], list[str]] = trans_rules,
+    dev_docs: list[str] = devDocs,
+    test_docs: list[str] = testDocs,
+    save_rules_to_file: bool = False,
+    saving_path: str = "../data/",
+) -> None:
     # Load and preprocess data
     logger.info("Loading TB-Dense data...")
     path = "tb_dense"
@@ -87,82 +113,119 @@ def process_tb_dense(trans_rules: dict[tuple[str, str], list[str]] = trans_rules
     tb_dense_docs = list(tb_dense_df.doc_id.unique())
     logger.info(f"There are {len(tb_dense_docs)} documents with {len(tb_dense_df)} relations in total.")
 
+    dev_docs = [doc.replace('.tml', '') for doc in dev_docs]
+    test_docs = [doc.replace('.tml', '') for doc in test_docs]
+    train_docs = [doc for doc in tb_dense_docs if doc not in dev_docs and doc not in test_docs]
+
     # Replace relation abbreviations with full names
     rel = {"s": "simultaneous", "i": "includes", "a": "after", "v": "vague", "ii": "is included", "b": "before"}
     tb_dense_df["relation"].replace(rel, inplace=True)
 
-    # Get pairs and relations of each document
-    logger.info("Processing document pairs and relations...")
-    doc_pair_relations = []
-    for doc_id in tb_dense_df.doc_id.unique():
-        doc_tlinks = tb_dense_df.loc[tb_dense_df["doc_id"] == doc_id]
-        doc_pairs = list(zip(doc_tlinks.event1_id.to_list(), doc_tlinks.event2_id.to_list()))
-        doc_relations = doc_tlinks.relation.to_list()
-        doc_pair_relations.append(dict(zip(doc_pairs, doc_tlinks.relation.to_list())))
+    train_df = tb_dense_df[tb_dense_df.doc_id.isin(train_docs)]
+    dev_df = tb_dense_df[tb_dense_df.doc_id.isin(dev_docs)]
+    test_df = tb_dense_df[tb_dense_df.doc_id.isin(test_docs)]
 
-    # Calculate statistics
-    num_pairs = [len(p) for p in doc_pair_relations]
-    logger.info(f"The number of pairs in a report on average is {mean(num_pairs)}")
+    logger.info(f"Train data: {len(train_df)}")
+    logger.info(f"Dev data: {len(dev_df)}")
+    logger.info(f"Test data: {len(test_df)}")
 
-    # Get transitivity triples
-    logger.info("Finding transitivity triples...")
-    trans_counts = []
-    trans_pairs = []
-    num_trans = []
-    for pr in doc_pair_relations:
-        report_pairs = list(pr.keys())
-        t_count, t_pairs = check_trans_for_pairs(report_pairs)
-        trans_counts.append(t_count)
-        trans_pairs.append(t_pairs)
-        for triple in t_pairs:
-            num_trans.append(1)
+    for mode, df in [("train", train_df), ("dev", dev_df), ("test", test_df)]:
+        logger.info(f"Processing {mode} data...")
+        doc_pair_relations = []
+        for doc_id in df.doc_id.unique():
+            doc_tlinks = df.loc[df["doc_id"] == doc_id]
+            doc_pairs = list(zip(doc_tlinks.event1_id.to_list(), doc_tlinks.event2_id.to_list()))
+            doc_relations = doc_tlinks.relation.to_list()
+            doc_pair_relations.append(dict(zip(doc_pairs, doc_tlinks.relation.to_list())))
 
-    logger.info(f"Transitivity appears on average: {mean(trans_counts)}")
-    logger.info(f"There are {sum(num_trans)} transitivity triples")
+        # Calculate statistics
+        num_pairs = [len(p) for p in doc_pair_relations]
+        logger.info(f"The number of pairs in a report on average is {mean(num_pairs)}")
 
-    # Construct story_triplets
-    logger.info("Constructing story triplets...")
-    doc_story_triplets = create_story_triplets(doc_pair_relations)
+        # Get transitivity triples
+        logger.info("Finding transitivity triples...")
+        trans_counts = []
+        trans_pairs = []
+        num_trans = []
+        for pr in doc_pair_relations:
+            report_pairs = list(pr.keys())
+            t_count, t_pairs = check_trans_for_pairs(report_pairs)
+            trans_counts.append(t_count)
+            trans_pairs.append(t_pairs)
+            for triple in t_pairs:
+                num_trans.append(1)
 
-    # Construct objects_info
-    logger.info("Constructing objects info...")
-    doc_objects_info = create_object_info(tb_dense_df)
+        logger.info(f"Transitivity appears on average: {mean(trans_counts)}")
+        logger.info(f"There are {sum(num_trans)} transitivity triples")
 
-    # Construct chains
-    logger.info("Constructing chains...")
-    inverse = {
-        "before": "after",
-        "after": "before",
-        "includes": "is included",
-        "is included": "includes",
-        # "overlap": "overlap",
-        "simultaneous": "simultaneous",
-        "vague": "vague",
-    }
+        # Construct story_triplets
+        logger.info("Constructing story triplets...")
+        doc_story_triplets = create_story_triplets(doc_pair_relations)
 
-    doc_chains = create_chain(doc_pair_relations, trans_pairs, inverse)
+        # Construct objects_info
+        logger.info("Constructing objects info...")
+        doc_objects_info = create_object_info(df)
 
-    # Construct questions
-    logger.info("Constructing questions...")
-    relation_set = list(tb_dense_df.relation.unique())
-    doc_questions = []
+        # Construct chains
+        logger.info("Constructing chains...")
+        inverse = {
+            "before": "after",
+            "after": "before",
+            "includes": "is included",
+            "is included": "includes",
+            # "overlap": "overlap",
+            "simultaneous": "simultaneous",
+            "vague": "vague",
+        }
 
-    for doc_index, doc_id in enumerate(tb_dense_df.doc_id.unique()):
-        doc_tlinks = tb_dense_df.loc[tb_dense_df["doc_id"] == doc_id]
-        questions = []
-        q_id = 0
+        doc_chains = create_chain(doc_pair_relations, trans_pairs, inverse)
 
-        for index, row in doc_tlinks.iterrows():
-            query = (row["event1_id"], row["event2_id"])
+        # Construct questions
+        logger.info("Constructing questions...")
+        relation_set = list(df.relation.unique())
+        doc_questions = []
 
-            # Add YN questions (one for each relation)
-            yn_questions, yn_answers = create_yn(query, row["relation"], relation_set)
+        for doc_index, doc_id in enumerate(df.doc_id.unique()):
+            doc_tlinks = df.loc[df["doc_id"] == doc_id]
+            questions = []
+            q_id = 0
 
-            for i, yn_question in enumerate(yn_questions):
+            for index, row in doc_tlinks.iterrows():
+                query = (row["event1_id"], row["event2_id"])
+
+                # Add YN questions (one for each relation)
+                yn_questions, yn_answers = create_yn(query, row["relation"], relation_set)
+
+                for i, yn_question in enumerate(yn_questions):
+                    question_info = {
+                        "num_facts": doc_chains[doc_index][query]["num_facts"],
+                        "reasoning_steps": doc_chains[doc_index][query]["reasoning_steps"],
+                        "asked_relation": relation_set[i],
+                        "all_relations": [row["relation"]],
+                        "target_relation": [row["relation"]],
+                        "chain": doc_chains[doc_index][query]["chain"],
+                        "goal_chain": doc_chains[doc_index][query]["goal_chain"],
+                    }
+
+                    questions.append(
+                        {
+                            "q_id": q_id,
+                            "q_type": "YN",
+                            "query": query,
+                            "question_info": question_info,
+                            "question": yn_question,
+                            "answer": yn_answers[i],
+                            "candidate_answers": ["Yes", "No"],
+                        }
+                    )
+                    q_id += 1
+
+                # Add the FR question
+                question, answer = create_fr(query, row["relation"])
                 question_info = {
                     "num_facts": doc_chains[doc_index][query]["num_facts"],
                     "reasoning_steps": doc_chains[doc_index][query]["reasoning_steps"],
-                    "asked_relation": relation_set[i],
+                    "asked_relation": [row["relation"]],
                     "all_relations": [row["relation"]],
                     "target_relation": [row["relation"]],
                     "chain": doc_chains[doc_index][query]["chain"],
@@ -172,76 +235,51 @@ def process_tb_dense(trans_rules: dict[tuple[str, str], list[str]] = trans_rules
                 questions.append(
                     {
                         "q_id": q_id,
-                        "q_type": "YN",
+                        "q_type": "FR",
                         "query": query,
                         "question_info": question_info,
-                        "question": yn_question,
-                        "answer": yn_answers[i],
-                        "candidate_answers": ["Yes", "No"],
+                        "question": question,
+                        "answer": answer,
+                        "candidate_answers": relation_set,
                     }
                 )
                 q_id += 1
 
-            # Add the FR question
-            question, answer = create_fr(query, row["relation"])
-            question_info = {
-                "num_facts": doc_chains[doc_index][query]["num_facts"],
-                "reasoning_steps": doc_chains[doc_index][query]["reasoning_steps"],
-                "asked_relation": [row["relation"]],
-                "all_relations": [row["relation"]],
-                "target_relation": [row["relation"]],
-                "chain": doc_chains[doc_index][query]["chain"],
-                "goal_chain": doc_chains[doc_index][query]["goal_chain"],
-            }
+            doc_questions.append(questions)
 
-            questions.append(
-                {
-                    "q_id": q_id,
-                    "q_type": "FR",
-                    "query": query,
-                    "question_info": question_info,
-                    "question": question,
-                    "answer": answer,
-                    "candidate_answers": relation_set,
-                }
-            )
-            q_id += 1
+        # Construct facts info
+        logger.info("Constructing facts info...")
 
-        doc_questions.append(questions)
+        doc_facts_info = create_facts_info(doc_questions, inverse, trans_rules)
 
-    # Construct facts info
-    logger.info("Constructing facts info...")
+        # Build final data structure
+        logger.info("Building final data structure...")
+        data = build_data(list(df.doc_id.unique()), doc_story_triplets, doc_questions, doc_objects_info, doc_facts_info)
 
-    doc_facts_info = create_facts_info(doc_questions, inverse, trans_rules)
+        # Add story content from original articles
+        logger.info("Adding story content from articles...")
+        ARTICLE_PATH = "../data/timebank_1_2/data/extra/"
 
-    # Build final data structure
-    logger.info("Building final data structure...")
-    data = build_data(tb_dense_docs, doc_story_triplets, doc_questions, doc_objects_info, doc_facts_info)
+        for article in data:
+            filename = article.get("identifier") + ".tml"
+            filepath = os.path.join(ARTICLE_PATH, filename)
+            article_soup = parse_article(filepath)
+            t0_value = get_t0(article_soup)
+            clean_article = get_clean_article(article_soup)
+            article_header = f"Written on <EVENT> id=t0 >{t0_value}< </EVENT>"
+            full_article = article_header + " " + clean_article
+            article["story"] = [full_article]
 
-    # Add story content from original articles
-    logger.info("Adding story content from articles...")
-    ARTICLE_PATH = "../data/timebank_1_2/data/extra/"
+        # Save to JSON
+        logger.info("Saving data to JSON...")
+        save_json(saving_path, f"tb_dense_{mode}.json", data)
 
-    for article in data:
-        filename = article.get("identifier") + ".tml"
-        filepath = os.path.join(ARTICLE_PATH, filename)
-        article_soup = parse_article(filepath)
-        t0_value = get_t0(article_soup)
-        clean_article = get_clean_article(article_soup)
-        article_header = f"Written on <EVENT> id=t0 >{t0_value}< </EVENT>"
-        full_article = article_header + " " + clean_article
-        article["story"] = [full_article]
-
-    # Save to JSON
-    logger.info("Saving data to JSON...")
-    save_json(path, "tb_dense.json", data)
-
-    # Save rules
-    logger.info("Saving rules...")
-    save_rules(inverse, "symmetry")
+    if save_rules_to_file:
+        # Save rules
+        logger.info("Saving rules...")
+        save_rules(inverse, "symmetry")
 
     logger.info("TB-Dense processing completed successfully!")
-    return data
 
 
 if __name__ == "__main__":
